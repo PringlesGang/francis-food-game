c7bcc4a7e6d3708f57ad91533c013ef4f14c897d
PringlesGang <liamelderbroek123@gmail.com>
2024-11-18 21:10:23 +0100

    Added mean and mode sampling prediction methods

--- predictions.py
+++ predictions.py
@@ -6,12 +6,26 @@ import pandas as pd
 class PredictionType(Enum):
     SAMPLE_MEAN = 1
     UNIFORM_MEAN = 2
+    SAMPLE_MEDIAN = 3
+    SAMPLE_MODE = 4
 
 def getSampleMean(data: pd.DataFrame) -> float:
     if (data.size == 0):
         return 8
     
-    return data.mean().iloc[0]
+    return pd.to_numeric(data.mean()).iloc[0]
+
+def getSampleMedian(data: pd.DataFrame) -> float:
+    if (data.size == 0):
+        return 8
+    
+    return pd.to_numeric(data.median()).iloc[0]
+
+def getSampleMode(data: pd.DataFrame) -> float:
+    if (data.size == 0):
+        return 8
+    
+    return pd.to_numeric(data.mean()).iloc[0]
 
 def getUniformMean() -> float:
     return 10
@@ -20,6 +34,10 @@ def getSinglePrediction(predictionMethod: PredictionType, data: pd.DataFrame) ->
     match predictionMethod:
         case PredictionType.SAMPLE_MEAN:
             return getSampleMean(data)
+        case PredictionType.SAMPLE_MEDIAN:
+            return getSampleMedian(data)
+        case PredictionType.SAMPLE_MODE:
+            return getSampleMode(data)
         case _: # Uniform mean
             return getUniformMean()
 
